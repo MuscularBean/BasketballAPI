2e81533b75440de4b08abdd3173171199e843b86
MuscuarBean <email@example.com>
2024-07-02 20:33:49 -0700

    Initial Commit

---
+++ BasketballAPI/Controllers/BasketballController.cs
@@ -0,0 +1,62 @@
+﻿using BasketballAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+namespace BasketballAPI.Controllers
+{
+    public class BasketballController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public BasketballController(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IActionResult> Index(string season, string team_name)
+        {
+            if(string.IsNullOrEmpty(season) || string.IsNullOrEmpty(team_name))
+            {
+                return View(new List<PlayerInfo>());
+            }
+            
+            var URL = $"https://api.sportsdata.io/v3/nba/stats/json/PlayerSeasonStatsByTeam/{season}/{team_name}?key=25ae768b66674ac785301051450291af";
+            var response = await _client.GetStringAsync(URL);
+
+            if (string.IsNullOrEmpty(response))
+            {
+                return View(new List<PlayerInfo>());
+            }
+
+            var playersArray = JArray.Parse(response);
+
+            List<PlayerInfo> playerInfos = new List<PlayerInfo>();
+
+            string teamLogoUrl = "";
+
+            foreach (var player in playersArray)
+            {
+                PlayerInfo playerInfo = new PlayerInfo
+                {
+                    Name = player["Name"].ToString(),
+                    Position = player["Position"].ToString(),
+
+                    TwoPointersMade = player["TwoPointersMade"].Value<double>(),
+                    ThreePointersMade = player["ThreePointersMade"].Value<double>(),
+                    Assists = player["Assists"].Value<double>(),
+                    Rebounds = player["Rebounds"].Value<double>(),
+                    Steals = player["Steals"].Value<double>(),
+                    Turnovers = player["Turnovers"].Value<double>(),
+                    Points = player["Points"].Value<double>(),
+                    Games = player["Games"].Value<double>(),
+                   /* TeamLogoUrl = teamLogoUrl */
+                };
+
+                playerInfos.Add(playerInfo);
+            }
+            return View(playerInfos);
+        }
+    }
+}
---
+++ BasketballAPI/Models/PlayerInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace BasketballAPI.Models
+{
+    public class PlayerInfo
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public double TwoPointersMade { get; set; }
+        public double ThreePointersMade { get; set; }
+        public double Assists {  get; set; }
+        public double Rebounds { get; set; }
+        public double Steals { get; set; }
+        public double Turnovers { get; set; }
+        public double Points { get; set; }
+        public double Games { get; set; }
+      /*  public string TeamLogoUrl {  get; set; } */
+
+    }
+}
